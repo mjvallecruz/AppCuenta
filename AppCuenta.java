9c5eddf44d3cfaf01ea52b974865f2817976933e
Maria del Valle <mariajose.delvalledelacruz@educa.madrid.org>
2026-04-08 19:54:09 +0200

    Commit Inicial_03_08

---
+++ AppCuenta.java
@@ -0,0 +1,22 @@
+package personaCuenta;
+
+public class AppCuenta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Persona p1 = new Persona("Pepe", 40, "20202020A");
+	        Persona p2 = new Persona("Marta", 25, "30303030B");
+
+	        CuentaBancaria cuenta1 = new CuentaBancaria(1, p1);
+	        CuentaBancaria cuenta2 = new CuentaBancaria(2, p2);
+
+	        cuenta1.ingresar(100);
+	        cuenta2.ingresar(200);
+
+	        cuenta1.mostrarDatos();
+	        cuenta2.mostrarDatos();
+	        p1.mostrarDatos();
+	        p1.mostrarDatosConBordes();
+	}
+
+}
